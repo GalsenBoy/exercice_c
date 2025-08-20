31750c07000d51953d1eef21f6de366f219459da
GalsenBoy <bilaly.cissokho21@gmail.com>
2025-08-20 12:46:34 +0200

    ajout fichier gitignore

--- basic/largeri.c
+++ basic/largeri.c
@@ -1,12 +1,13 @@
 /*
-Write a C program to find the larger of two given integers. However if the two integers have the same remainder when divided by 5, choose the smaller integer. 
+Write a C program to find the larger of two given integers. However if the two integers have the same remainder when divided by 5, choose the smaller integer.
 If the two integers are the same, return 0.
 */
 #define MAX(i, j) (((i) > (j)) ? (i) : (j))
 #define MIN(i, j) (((i) < (j)) ? (i) : (j))
 #include <stdio.h>
 
-int larger_of_two_given_integers(int a, int b){
+int larger_of_two_given_integers(int a, int b)
+{
     if (a == b)
         return 0;
     if (a % 5 == b % 5)
@@ -14,13 +15,13 @@ int larger_of_two_given_integers(int a, int b){
     return MAX(a, b);
 }
 
-int main(){
-    int a,b;
+int main()
+{
+    int a, b;
     printf("Saisir une valeur\n");
-    scanf("%d",&a);
-     printf("Saisir une valeur\n");
-    scanf("%d",&b);
-    printf("Valeur de retour %d\n",larger_of_two_given_integers(a,b));
+    scanf("%d", &a);
+    printf("Saisir une valeur\n");
+    scanf("%d", &b);
+    printf("Valeur de retour %d\n", larger_of_two_given_integers(a, b));
     return 0;
 }
-
