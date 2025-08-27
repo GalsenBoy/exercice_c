2af7671190352a79d9920f142479ec4e86086382
GalsenBoy <bilaly.cissokho21@gmail.com>
2025-08-27 16:53:28 +0200

    Write a program in C for a 2D array of size 3x3 and print the matrix.

--- array/matrix_3_3.c
+++ array/matrix_3_3.c
@@ -7,25 +7,23 @@ Write a program in C for a 2D array of size 3x3 and print the matrix.
 #include <stdio.h>
 
 int main(){
-    int matrix[3][3],i,j;
-    for (i = 0; i < 3; i++)
+    int matrix[SIZE][SIZE],i,j;
+    for (i = 0; i < SIZE; i++)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < SIZE; j++)
         {
             printf("Saisir matrix [%d] [%d]",i,j);
             scanf("%d",&matrix[i][j]);
         }
     }
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < SIZE; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < SIZE; j++)
         {
             printf("%d\t",matrix[i][j]);
         }
         printf("\n");
     }
-    
-    
     return 0;
 }
